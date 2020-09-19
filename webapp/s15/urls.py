e45141c3946db8e85b04f40a2cacf2338f736cce
Tomasz Karbownicki <tomasz@karbownicki.com>
2020-09-19 09:52:58 +0200

    Adding debug toolbar urls if available

--- webapp/s15/urls.py
+++ webapp/s15/urls.py
@@ -29,7 +29,10 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except:
+        pass
